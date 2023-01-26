190d089a34240254474efe4ec59c09ae5815023b
AishwaryaKamble96 <aishwaryakamblegem@gmail.com>
2023-01-26 11:01:21 +0100

    fea:added show button and bookmared

--- quiz_app/js/index.js
+++ quiz_app/js/index.js
@@ -1,6 +1,25 @@
 const showAnswerButton = document.querySelector('[data-js="show-button"]');
 const answerValue = document.querySelector('[data-js="answer"]');
 
+const bookmarkImg = document.querySelector('[ data-js="page-bookmark"]');
+
+// Show buttonevent listner
 showAnswerButton.addEventListener("click", () => {
   answerValue.classList.toggle("show");
+  console.log(showAnswerButton.textContent);
+  if (showAnswerButton.textContent === "Show Answer") {
+    showAnswerButton.textContent = "Hide Answer";
+  } else {
+    showAnswerButton.textContent = "Show Answer";
+  }
+});
+
+//Event listner for bookmark button
+bookmarkImg.addEventListener("click", (e) => {
+  const bookmarkValue = bookmarkImg.getAttribute("src");
+  if (bookmarkValue === "./assest/bookmarked.png") {
+    bookmarkImg.setAttribute("src", "./assest/bookmark.png");
+  } else {
+    bookmarkImg.setAttribute("src", "./assest/bookmarked.png");
+  }
 });
