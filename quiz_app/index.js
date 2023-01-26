c464f304e658c866800bf9b204670acea911c63c
AishwaryaKamble96 <aishwaryakamblegem@gmail.com>
2023-01-26 09:51:14 +0100

    moved js and css file to folder

--- quiz_app/index.js
+++
@@ -1,6 +0,0 @@
-const showAnswerButton = document.querySelector('[data-js="show-button"]');
-const answerValue = document.querySelector('[data-js="answer"]');
-
-showAnswerButton.addEventListener("click", () => {
-  answerValue.classList.toggle("show");
-});
